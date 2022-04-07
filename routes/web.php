91c0fbef98c9226daee6b430f3fb21d4e8a52043
kaarelnoole <kaarelnoole@gmail.com>
2022-04-07 10:08:46 +0300

    Add css, post and posts.blade.php and changed web

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
-Route::get('/posts/{post}', function ($slug){
-    $path = __DIR__."/../resources/posts/{$slug}.html";
-    if (! file_exists($path)){
-        return redirect("/");
-}
-$post = cache()->remember("posts.{$slug}",now()->addminutes(5), fn() => file_get_contents($path));
-return view('post', ["post"=>$post]);
-})->where("post","[A-z_\-]+");
+Route::get('post', function (){
+    return view('post');
+});
 
 Route::get('/hello',function ()
 {
