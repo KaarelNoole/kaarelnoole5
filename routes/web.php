4b648e9e8cd5366f674eb60daca88f375f64b881
kaarelnoole <kaarelnoole@gmail.com>
2022-05-19 10:00:29 +0300

    ep 12 done

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,14 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
 $posts = collect(File::files(resource_path("posts")))
-    ->map(fn($file)=> YamlFrontMatter::parseFile($file));
+    ->map(fn($file)=> YamlFrontMatter::parseFile($file))
     ->map(fn($document) => new Post(
        $document->title,
        $document->excerpt,
        $document->date,
        $document->body(),
        $document->slug
-        
+
 ));
 
 
