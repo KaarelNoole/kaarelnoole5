ab5652357ba930a736a7a1597c5cb73e24f57e0b
kaarelnoole <kaarelnoole@gmail.com>
2022-06-09 11:53:02 +0300

    Search (The Cleaner Way)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
@@ -18,25 +19,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
-    $posts = Post::latest();
-
-    if(request('search')) {
-        $posts
-        ->where('title', 'like', '%' . request('search') . '%')
-        >orWhere('body', 'like', '%' . request('search') . '%');
-    }
-return view('posts', [
-    'posts' => $posts->get(),
-    'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('/posts/{post:slug}', function (Post $post){
-        return view('post',[
-            'post' => $post
-    ]);
-});
+Route::get('/', [PostController::class, 'index']) ->name('home');
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function(Category $category){
     return view('posts',[
